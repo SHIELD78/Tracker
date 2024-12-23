c15cb7e5b6e3e83b313a26e2a1f08fba51155dc4
Ritik Rana <ritikrana8596@gmail.com>
2024-12-23 23:43:50 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
 const express = require('express');
-const app = express();
-const port = 3000; // Or any port you want to use
+const cors = require('cors');
 
-app.use(express.json()); // To parse JSON data
+const app = express();
+const port = 3000;
+
+app.use(cors()); // Allow all origins
+app.use(express.json()); // For parsing application/json
 
 // Endpoint to receive the location
 app.post('/receive-location', (req, res) => {
     const { latitude, longitude } = req.body;
-
-    // Log the received location (you can store this in a database or process it)
     console.log(`Received Location: Latitude: ${latitude}, Longitude: ${longitude}`);
-
-    // Send a response back
     res.json({ message: 'Location received successfully' });
 });
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
+
