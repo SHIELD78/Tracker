8b47ab111e44dfaf802381fc2f9f398fae8139bb
Ritik Rana <ritikrana8596@gmail.com>
2024-12-24 20:04:31 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const cors = require('cors');
+const https = require('https');
+const fs = require('fs');
+
+// Self-signed certificates (or use ngrok URL for HTTPS)
+const options = {
+    key: fs.readFileSync('path/to/your/private-key.pem'),
+    cert: fs.readFileSync('path/to/your/certificate.pem')
+};
 
 const app = express();
 const port = 3000;
@@ -14,7 +22,7 @@ app.post('/receive-location', (req, res) => {
     res.json({ message: 'Location received successfully' });
 });
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}`);
+// Create an HTTPS server
+https.createServer(options, app).listen(port, () => {
+    console.log(`Server is running on https://localhost:${port}`);
 });
-
