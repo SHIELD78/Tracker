142f2f846cdddcbbc96b8da3aeeb47716f0881af
Ritik Rana <ritikrana8596@gmail.com>
2024-12-27 12:44:32 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -4,16 +4,20 @@ const cors = require('cors');
 const app = express();
 const port = 3000;
 
-app.use(cors()); // Allow all origins
-app.use(express.json()); // For parsing application/json
+// Middleware
+app.use(cors()); // Allow cross-origin requests
+app.use(express.json()); // Parse JSON body
 
 // Endpoint to receive the location
 app.post('/receive-location', (req, res) => {
     const { latitude, longitude } = req.body;
     console.log(`Received Location: Latitude: ${latitude}, Longitude: ${longitude}`);
+    
+    // Respond with a success message
     res.json({ message: 'Location received successfully' });
 });
 
+// Start the server
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
